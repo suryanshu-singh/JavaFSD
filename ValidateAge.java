0688fc0308f610f6e6dce772dfde9d16539e44b3
suryanshu-singh <33225159+suryanshu-singh@users.noreply.github.com>
2020-02-04 10:29:02 +0530

    Add files via upload

---
+++ ValidateAge.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class InvalidAgeException extends Exception{
+	void ageException()
+	{
+		System.out.println("Age not valid!!");
+	}
+}
+class ValidateAge{
+		void checkAge(int n) throws InvalidAgeException{
+				if(n<=15){
+					throw new InvalidAgeException();
+				}
+				else{
+					System.out.println("Age is valid!!");
+				}
+			}
+	public static void main(String[] args) throws MyException {
+		System.out.println("Enter the age to validate:");
+		Scanner scan = new Scanner(System.in);
+		try{
+			Integer input=scan.nextInt();
+			new ValidateAge().checkAge(input);
+		}catch(InvalidAgeException e){
+			new InvalidAgeException().ageException();
+		}
+	}
+}
